b79e0b038af31b57441698fea7b8947da936f026
Ali Kirecligol <alikirecligol@gmail.com>
2020-02-04 19:50:55 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,11 @@ import app from './app'
 //Connects to the Database -> then starts the express
 createConnection()
   .then(async (connection) => {
-      app.listen(3000, () => {
+      app.listen(process.env.PORT || 3000, () => {
           console.log('Server started on port 3000!');
       });
   })
-  .catch(error => console.log(error));
+  .catch((error) => {
+    console.log(error);
+    console.log(process.env.TYPEORM_DATABASE);
+  });
